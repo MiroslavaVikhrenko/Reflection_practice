fd994388ebc27c5ffb9f2bee3cdbaca83f874748
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-02 17:54:21 -0600

    Practice reflection

---
+++ 20250602_Task5/AutoMapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250602_Task5
+{
+    public static class AutoMapper
+    {
+        public static TTarget Map<TSource, TTarget>(TSource source)
+    where TTarget : new()
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            TTarget target = new TTarget();
+
+            var sourceProps = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var targetProps = typeof(TTarget).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var sourceProp in sourceProps)
+            {
+                var targetProp = Array.Find(targetProps, p =>
+                    p.Name == sourceProp.Name &&
+                    p.PropertyType == sourceProp.PropertyType &&
+                    p.CanWrite);
+
+                if (targetProp != null)
+                {
+                    var value = sourceProp.GetValue(source);
+                    targetProp.SetValue(target, value);
+                }
+            }
+
+            return target;
+        }
+    }
+}
---
+++ 20250602_Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _20250602_Task5
+{
+    /*
+     Используя рефлексию, создать собственный AutoMapper, для маппинга свойств двух классов:
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker("Alice", "Smith", 30, "Engineer");
+
+            var client = AutoMapper.Map<Worker, Client>(worker);
+
+            Console.WriteLine("Mapped Client object:");
+            Console.WriteLine($"FirstName: {client.FirstName}");
+            Console.WriteLine($"LastName : {client.LastName}");
+            Console.WriteLine($"Age      : {client.Age}");
+            Console.WriteLine($"Email    : {client.Email ?? "[null]"}");
+
+            Console.ReadLine();
+        }
+    }
+}
